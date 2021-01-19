53a2a29a8880f5b2da23b6addc308a1fda2fe78b
StarGolx <StarGolx@users.noreply.github.com>
2021-01-19 08:04:26 +0100

    remove

--- HTML/自己编写的/起始页/js/script.js
+++
@@ -1,6 +0,0 @@
-function showSettings() {
-    document.getElementById("settings").style.display = "block";
-}
-function hideSettings() {
-    document.getElementById("settings").style.display = "none";
-}
--- HTML/自己编写的/起始页/js/search.js
+++
@@ -1,5 +0,0 @@
-function search() {
-    var searchValue = document.getElementById("searchInput").value;
-    // alert(searchValue);
-    window.location.href = "http://www.baidu.com/s?ie=UTF-8&wd=" + searchValue;
-}
